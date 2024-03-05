5bd08db620c22fa6ea9b34509656ce86346a17de
anayuaida@gmail.com <yuaida.12@gmail.com>
2024-03-05 13:15:47 +0700

    helper function handleAjax

--- resources/views/pages/konfigurasi/menu.blade.php
+++ resources/views/pages/konfigurasi/menu.blade.php
@@ -27,73 +27,107 @@
         {!! $dataTable->scripts() !!}
 
         <script>
-            $('.add').on('click', function(e){
-                e.preventDefault();
+            function handleFormSubmit(selector) {  
 
-                $.ajax({
-                    url: this.href,
-                    method: 'get',
-                    beforeSend: function(){
-                        showLoading()
-                    },
-                    complete: function(){
-                        showLoading(false)
-                    },
-                    success: function(res){
-                       const modal = $('#modal_action')
-                       modal.html(res)
-                       modal.modal('show')
-
-                       $('[name="level_menu"]').on('change', function(){
-                           console.log(this.value)
-                            if(this.value == 'sub_menu'){
-                                $('#main_menu_wrapper').removeClass('d-none')
-                            }else{
-                                $('#main_menu_wrapper').addClass('d-none')
-                            }
-                       })
-
-                       $('#form_action').on('submit', function(e){
-                           e.preventDefault();
-                           const _form = this
-                           $.ajax({
+                function init() {
+                    $(selector).on('submit', function(e){
+                            e.preventDefault();
+                            const _form = this
+                            $.ajax({
                                 url: this.action,
-                                method: this.method,
-                                data: new FormData(_form),
+                                method: this.method,                        data: new FormData(_form),
                                 contentType: false,
                                 processData: false,
                                 beforeSend: function() {
-                                   $(_form).find('.is-invalid').removeClass('is-invalid')
-                                   $(_form).find('.invalid-feedback').remove()
-                                   submitLoader().show()
+                                    $(_form).find('.is-invalid').removeClass('is-invalid')
+                                    $(_form).find('.invalid-feedback').remove()
+                                    submitLoader().show()
                                 },
-                                
+                                            
                                 success: function(res){
-                                   $('#modal_action').modal('hide')
-                                   window.LaravelDataTables['menu-table'].ajax.reload()
+                                    $('#modal_action').modal('hide')
+                                    window.LaravelDataTables['menu-table'].ajax.reload()
                                 },
                                 complete: function(){
                                     submitLoader().hide()
                                 },
                                 error: function(err) {
-                                   const errors = err.responseJSON?.errors
+                                    const errors = err.responseJSON?.errors
 
-                                   if(errors){
-                                    for (let [key, message] of Object.entries(errors)) {
-                                        console.log(key, message);
-                                        $(`[name="${key}"]`).addClass('is-invalid')
-                                        .parent()
-                                        .append(`<div class="invalid-feedback">${message}</div>`)
+                                    if(errors){
+                                        for (let [key, message] of Object.entries(errors)) {
+                                            $(`[name="${key}"]`).addClass('is-invalid')
+                                            .parent()
+                                            .append(`<div class="invalid-feedback">${message}</div>`)
+                                        }
                                     }
-                                   }
                                 }
-                           })
-                       })
-                    },
-                    error: function(err){
-                        console.log(err);
-                    }
-                })
+                            })
+                    })
+                }
+
+                return {
+                    init
+                }
+            }
+            function handleAjax(url, method = 'get') {
+                function onSuccess(cb, runDefault = true) {
+                    this.onSuccessCallback = cb
+                    this.runDefaultSuccessCallback = runDefault
+                    // return this
+                }
+
+                function execute(){
+                    $.ajax({
+                        url,
+                        method,
+                        beforeSend: function(){
+                            showLoading()
+                        },
+                        complete: function(){
+                            showLoading(false)
+                        },
+                        success: (res) => {
+                            if (this.runDefaultSuccessCallback) {
+                                const modal = $('#modal_action')
+                                modal.html(res)
+                                modal.modal('show')
+                            }
+                            
+                            this.onSuccessCallback && this.onSuccessCallback()
+                        
+                        },
+                        error: function(err){
+                            console.log(err);
+                        }
+                    })
+                }
+
+                function onError(cb){
+                    this.onErrorCallback = cb
+                    return this
+                }
+
+                return {
+                    execute,
+                    onSuccess,
+                    runDefaultSuccessCallback: true
+                }
+            }
+
+            $('.add').on('click', function(e) {
+                e.preventDefault();
+
+                handleAjax(this.href).onSuccess(function() {
+                    $('[name="level_menu"]').on('change', function(){
+                        if(this.value == 'sub_menu'){
+                            $('#main_menu_wrapper').removeClass('d-none');
+                        } else {
+                            $('#main_menu_wrapper').addClass('d-none');
+                        }
+                    });
+                    handleFormSubmit('#form_action');
+                }).execute();
             })
         </script>
     @endpush
