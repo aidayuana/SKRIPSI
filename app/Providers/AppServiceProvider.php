46ed149957383cc5ba9619118df19b09287e90be
anayuaida@gmail.com <yuaida.12@gmail.com>
2024-03-11 12:33:52 +0700

    enforce polymorphism relation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        Relation::enforceMorphMap([
+            'users' => User::class,
+        ]);
     }
 }
